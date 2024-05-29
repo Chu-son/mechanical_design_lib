742eee908fd74732c81424aa41e7b878ca5eec37
Chu-son <chuson.dev@gmail.com>
2024-05-29 15:25:00 +0000

    add LinearActuatorFactory

--- src/mechanical_design_lib/actuator.py
+++ src/mechanical_design_lib/actuator.py
@@ -1,7 +1,6 @@
 import sympy
 import numpy as np
 
-
 G = 9.81  # m/s^2
 
 
@@ -45,17 +44,26 @@ class LinearActuator:
         acceleration = acceleration or self._max_acceleration
         deceleration = deceleration or self._max_deceleration
 
-        t_accel = velocity / acceleration  # s
-        dx_accel = velocity * t_accel / 2  # mm
+        # Define symbols
+        t_accel = sympy.Symbol('t_accel')
+        t_decel = sympy.Symbol('t_decel')
+        t_const = sympy.Symbol('t_const')
 
-        t_decel = velocity / deceleration  # s
-        dx_decel = velocity * t_decel / 2  # mm
+        # Define equations
+        eq1 = sympy.Eq(velocity, acceleration * t_accel)
+        eq2 = sympy.Eq(velocity, deceleration * t_decel)
+        eq3 = sympy.Eq(target_position, (velocity * t_accel / 2) +
+                       velocity * t_const + (velocity * t_decel / 2))
 
-        dx_const = target_position - dx_accel - dx_decel  # mm
-        t_const = dx_const / velocity  # s
+        # Solve equations
+        solution = sympy.solve((eq1, eq2, eq3), (t_accel, t_decel, t_const))
+        print(f"Solutions: {solution}")
+
+        t_accel = solution[t_accel]
+        t_decel = solution[t_decel]
+        t_const = solution[t_const]
 
         t_total = t_accel + t_const + t_decel  # s
-        x_total = dx_accel + dx_const + dx_decel  # mm
 
         delta_t = 0.01  # s
         t_array = np.arange(0, t_total, delta_t)
@@ -72,14 +80,91 @@ class LinearActuator:
         return np.array([t_array, v_array])
 
 
+class LinearActuatorFactory:
+    @staticmethod
+    def create_linear_actuator(stroke: int,  # mm
+                               time: float,  # s
+                               max_velocity: int | None = None,  # mm/s
+                               max_acceleration: float | None = None,  # mm/s^2
+                               max_deceleration: float | None = None,  # mm/s^2
+                               ) -> LinearActuator:
+
+        t_accel = sympy.Symbol('t_accel')
+        t_decel = sympy.Symbol('t_decel')
+
+        if max_velocity is not None:
+            max_acceleration = UnitConverter.g_to_mm_s2(0.3)
+            max_deceleration = UnitConverter.g_to_mm_s2(0.3)
+
+            # TODO
+            # # Calculate max_acceleration
+            # if max_acceleration is None and max_deceleration is None:
+            #     max_acceleration = sympy.Symbol('max_acceleration')
+            #     max_deceleration = max_acceleration
+            # else:
+            #     max_acceleration = max_acceleration or sympy.Symbol(
+            #         'max_acceleration')
+            #     max_deceleration = max_deceleration or sympy.Symbol(
+            #         'max_deceleration')
+
+            # eq1 = sympy.Eq(max_velocity, t_accel * max_acceleration)
+            # eq2 = sympy.Eq(max_velocity, t_decel * max_deceleration)
+            # eq3 = sympy.Eq(stroke, (max_velocity * t_accel / 2) + max_velocity *
+            #                (time - t_accel - t_decel) + (max_velocity * t_decel / 2))
+
+            # solution = sympy.solve(
+            #     (eq1, eq2, eq3), (max_acceleration), dict=True)
+
+            # print(f"Solutions: {solution}")
+
+            # if type(solution) is list:
+            #     max_acceleration = solution[0][max_acceleration]
+            #     max_deceleration = solution[0][max_deceleration]
+            #     print(f"max_accel[G]: {max_acceleration * 1000 / G}")
+            # else:
+            #     max_acceleration = solution[max_acceleration]
+            #     max_deceleration = solution[max_deceleration]
+
+        elif max_acceleration is None or max_deceleration is None:
+            # Calculate max_velocity with max_acceleration and max_deceleration as 0.3G
+            max_acceleration = max_acceleration or UnitConverter.g_to_mm_s2(
+                0.3)
+            max_deceleration = max_deceleration or UnitConverter.g_to_mm_s2(
+                0.3)
+
+            max_velocity = sympy.Symbol('max_velocity')
+            eq1 = sympy.Eq(max_velocity, t_accel * max_acceleration)
+            eq2 = sympy.Eq(max_velocity, t_decel * max_deceleration)
+            eq3 = sympy.Eq(stroke, (max_velocity * t_accel / 2) + max_velocity *
+                           (time - t_accel - t_decel) + (max_velocity * t_decel / 2))
+
+            solution = sympy.solve(
+                (eq1, eq2, eq3), (t_accel, t_decel, max_velocity), dict=True)
+
+            print(f"Solutions: {solution}")
+            if type(solution) is list:
+                max_velocity = solution[0][max_velocity]
+            else:
+                max_velocity = solution[max_velocity]
+
+        else:
+            raise ValueError(
+                "Either max_velocity or max_acceleration/max_deceleration must be provided.")
+
+        return LinearActuator(stroke, max_velocity, max_acceleration, max_deceleration)
+
+
 if __name__ == "__main__":
-    actuator = LinearActuator(stroke=600, max_velocity=120,
-                              max_acceleration=UnitConverter.g_to_mm_s2(0.3),
-                              max_deceleration=UnitConverter.g_to_mm_s2(0.3)
-                              )
+    # actuator = LinearActuator(stroke=600, max_velocity=120,
+    #                           max_acceleration=UnitConverter.g_to_mm_s2(0.3),
+    #                           max_deceleration=UnitConverter.g_to_mm_s2(0.3)
+    #                           )
+    # actuator = LinearActuatorFactory.create_linear_actuator(stroke=600, time=3)
+    actuator = LinearActuatorFactory.create_linear_actuator(
+        stroke=600, time=4, max_velocity=120)
     move_log = actuator.move(target_position=600, only_simulation=True,
-                             acceleration=UnitConverter.g_to_mm_s2(0.1),
-                             deceleration=UnitConverter.g_to_mm_s2(0.2)
+                             #  acceleration=UnitConverter.g_to_mm_s2(0.1),
+                             #  deceleration=UnitConverter.g_to_mm_s2(0.2)
                              )
 
     import matplotlib.pyplot as plt
