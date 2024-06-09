00931653c7a837675382b98e78b20d3dc23130f9
Chu-son <chuson.dev@gmail.com>
2024-06-09 13:44:29 +0000

    update

--- src/mechanical_design_lib/actuator/stepping_motor.py
+++ src/mechanical_design_lib/actuator/stepping_motor.py
@@ -1,9 +1,13 @@
+import dataclasses
 import sympy
+from IPython.display import display
 
-from mechanical_design_lib.utils.unit import UnitConverter
+from mechanical_design_lib.utils.unit import UnitConverter, UnitSymbol
 
 
 class SteppingMotor:
+
+
     def __init__(self,
                  phase: int,
                  microstep_resolution: int,  # 1: full step, 2: half step, 4: quarter step,,,
@@ -28,11 +32,60 @@ class SteppingMotor:
 
     def get_rpm(self, pps: float) -> float:
         return (pps / (360 / self.step_angle)) * 60
-    
+
     def get_pulse(self, revolute_angle: float) -> float:
         return revolute_angle / self.step_angle
 
+class StartingPulseRate:
+    @dataclasses.dataclass
+    class Symbols:
+        fs: UnitSymbol
+        jo: UnitSymbol
+        jl: UnitSymbol
 
+        def display(self):
+            display(self.fs.symbol)
+
+    class Formula:
+        f: sympy.Symbol
+
+    def __init__(self,
+                    fs: float,  # Hz
+                    jo: float,  # kg*m^2
+                    jl: float,  # kg*m^2
+                    ):
+
+        self._fs = fs
+        self._jo = jo
+        self._jl = jl
+
+        self._symbols = self.Symbols(
+            fs=UnitSymbol('fs', 'Hz'),
+            jo=UnitSymbol('jo', 'kg*m^2'),
+            jl=UnitSymbol('jl', 'kg*m^2'),
+        )
+
+    def _formula(self):
+        s = self._symbols
+        f = s.fs / sympy.sqrt(1 + (s.jl / s.jo))
+
+        self._formula = self.Formula(
+            f=f
+        )
+
+    def display_symbols(self):
+        self._symbols.display()
+
+    def display_formula(self):
+        sympy.pprint(self._formula)
+
+    def calculate(self):
+        self._formula()
+        return self._formula.f.subs({
+            self._symbols.fs: self._fs,
+            self._symbols.jo: self._jo,
+            self._symbols.jl: self._jl,
+        })
 
 if __name__ == '__main__':
     motor = SteppingMotor(phase=2, microstep_resolution=1)
--- src/mechanical_design_lib/utils/unit.py
+++ src/mechanical_design_lib/utils/unit.py
@@ -1,10 +1,69 @@
 import enum
 import sympy
 
-G = 9.81  # m/s^2
+from mechanical_design_lib.utils.constant import GRAVITY as G
 
 
 class UnitConverter:
     @staticmethod
     def g_to_mm_s2(acceleration_g: float) -> float:
         return acceleration_g * G * 1000
+
+
+class SIPrefix(enum.Enum):
+    YOTTA = 24
+    ZETTA = 21
+    EXA = 18
+    PETA = 15
+    TERA = 12
+    GIGA = 9
+    MEGA = 6
+    KILO = 3
+    HECTO = 2
+    DECA = 1
+    NONE = 0
+    DECI = -1
+    CENTI = -2
+    MILLI = -3
+    MICRO = -6
+    NANO = -9
+    PICO = -12
+    FEMTO = -15
+    ATTO = -18
+    ZEPTO = -21
+    YOCTO = -24
+
+
+class SIPrefixedValue:
+    def __init__(self, value: float, prefix: SIPrefix):
+        self._value = value
+        self._prefix = prefix
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    def convert_to(self, prefix: SIPrefix) -> float:
+        return self.value * 10 ** (self.prefix.value - prefix.value)
+
+    @property
+    def prefix(self) -> SIPrefix:
+        return self._prefix
+
+    def __str__(self) -> str:
+        return f"{self.value} [{self.prefix.name}]"
+
+
+class UnitSymbol:
+    def __init__(self, name: str, unit: str):
+        self._symbol = sympy.Symbol(name)
+        self._unit = sympy.Symbol(unit)
+
+
+    @property
+    def unit(self) -> sympy.Symbol:
+        return self._unit
+
+    @property
+    def symbol(self) -> sympy.Symbol:
+        return self._symbol
