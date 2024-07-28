9d8c7c514400dc581f86fb04b0e77b74c2b5dff9
Chu-son <chuson.dev@gmail.com>
2024-07-28 16:15:13 +0000

    refactor: Refactor Flowchart class to use ElementIterator for post-processing

--- src/mechanical_design_lib/machine/machine.py
+++ src/mechanical_design_lib/machine/machine.py
@@ -2,46 +2,40 @@ import mechanical_design_lib.utils.flowchart as flowchart
 from mechanical_design_lib.actuator.actuator import BaseActuator
 
 
-class Machine:
-    def __init__(self):
-        self.units = []
-
-    def add_unit(self, unit):
-        self.units.append(unit)
-
-
-class MachineUnit:
-    def __init__(self, name: str):
-        self._name = name
-        self.behaviors = {}
-
-    def add_behavior(self, behavior):
-        self.behaviors[behavior.name] = behavior
-
-
-class MachineUnitBehavior:
-    def __init__(self, name: str):
-        self._name = name
-        self.behavior_summaries = []
-
-    def add_behavior_summary(self, behavior_summary):
-        self.behavior_summaries.append(behavior_summary)
-
-    @property
-    def name(self):
-        return self._name
-
-
 class BehaviorSummary(flowchart.Subroutine):
     def __init__(self, description: str,
                  root_element: flowchart.FlowchartElement = None,
-                 is_parse_subroutine: bool = False
+                 is_parse_subroutine: bool = False,
+                 takt_time: float = None,
                  ):
         super().__init__(description,
                          subroutine_root_element=root_element,
                          is_parse_subroutine=is_parse_subroutine
                          )
         self._description = description
+        self._takt_time = takt_time
+
+    def get_takt_time(self,
+                      parse_subroutine: bool = True,
+                      ) -> float:
+        if parse_subroutine and self.subroutine_root_element is not None:
+            return self._calc_takt_time()
+
+        elif self._takt_time is not None:
+            return self._takt_time
+        else:
+            raise ValueError("Takt time is not set.")
+
+    def _calc_takt_time(self) -> float:
+        takt_time = 0
+        for element in flowchart.ElementIterator(self.subroutine_root_element):
+            if hasattr(element, "get_takt_time"):
+                tt = element.get_takt_time()
+                if tt is None:
+                    continue
+                takt_time += tt
+
+        return takt_time
 
 
 class BehaviorDetailAction(flowchart.Action):
@@ -49,56 +43,207 @@ class BehaviorDetailAction(flowchart.Action):
     #     super().__init__(action)
     #     self._actuator = actuator
 
-    def __init__(self, action: str, action_time: float = None):
+    def __init__(self,
+                 action: str,
+                 takt_time: float = None,
+                 ):
         super().__init__(action)
-        self._action_time = action_time
+        self._takt_time = takt_time
+
+    @property
+    def label(self):
+        return f"{self._label}\n (takt time: {self._takt_time})"
+
+    def get_takt_time(self) -> float:
+        if self._takt_time is None:
+            print(f"Warning: Takt time is not set for {self.label}")
+
+        print(f"Action: {self._label}, takt time: {self._takt_time}")
+        return self._takt_time
+
+
+class BehaviorParallel(flowchart.Parallel):
+    def __init__(self, description: str):
+        super().__init__(description)
+
+    def add_parallel_element(self, root_element: BehaviorDetailAction):
+        return super().add_parallel_element(root_element)
+
+    def get_takt_time(self) -> float:
+        takt_time = 0
+        for element in self._parallel_elements:
+            element_takt_time = 0
+            for e in flowchart.ElementIterator(element):
+                if hasattr(e, "get_takt_time"):
+                    tt = e.get_takt_time()
+                    if tt is None:
+                        continue
+                    element_takt_time += tt
+
+            takt_time = max(takt_time, element_takt_time)
+
+        print(f"Parallel: {self.label}, max takt time: {takt_time}")
+        return takt_time
+
+
+class BehaviorLoop(flowchart.Loop):
+    def __init__(self, description: str, loop_count: int):
+        super().__init__(description, loop_count)
+
+    def set_loop_content(self, root_element: BehaviorDetailAction):
+        return super().set_loop_content(root_element)
+
+    def get_takt_time(self) -> float:
+        takt_time = 0
+        for element in flowchart.ElementIterator(self._loop_content):
+            if hasattr(element, "get_takt_time"):
+                tt = element.get_takt_time()
+                if tt is None:
+                    continue
+                takt_time += tt
+
+        takt = takt_time * self._loop_count
+        print(f"Loop: {self.label}, takt time: {
+              takt_time} x {self._loop_count} = {takt}")
+        return takt
+
+
+class BehaviorDecision(flowchart.Decision):
+    def __init__(self, description: str, default_yes: bool = True):
+        super().__init__(description)
+
+        self._default_yes = default_yes
+
+    def get_takt_time(self) -> float:
+        takt_time = 0
+
+        if self._default_yes:
+            for element in flowchart.ElementIterator(self._yes_root_element):
+                if hasattr(element, "get_takt_time"):
+                    tt = element.get_takt_time()
+                    if tt is None:
+                        continue
+                    takt_time += tt
+
+        else:
+            for element in flowchart.ElementIterator(self._no_root_element):
+                if hasattr(element, "get_takt_time"):
+                    tt = element.get_takt_time()
+                    if tt is None:
+                        continue
+                    takt_time += tt
+
+        print(f"Decision: {self.label}, Value: {
+              "Yes" if self._default_yes else "No"}, takt time: {takt_time}")
+        return takt_time
+
+
+class MachineUnit:
+    def __init__(self, name: str):
+        self._name = name
+        self._behaviors = {}
+
+    def add_behavior(self, behavior_name: str, behavior: BehaviorSummary):
+        if behavior_name in self._behaviors:
+            raise ValueError(f"Behavior {behavior_name} already exists.")
+
+        self._behaviors[behavior_name] = behavior
+        return self
+
+    def get_behavior(self, behavior_name: str):
+        if behavior_name not in self._behaviors:
+            raise ValueError(f"Behavior {behavior_name} does not exist.")
+        return self._behaviors[behavior_name]
+
+
+class Machine:
+    def __init__(self):
+        self.units = {}
+
+    def add_unit(self, unit: MachineUnit):
+        if unit._name in self.units:
+            raise ValueError(f"Unit {unit._name} already exists.")
+
+        self.units[unit._name] = unit
+        return self
 
 
 if __name__ == "__main__":
 
-    # フローチャートの作成例
+    # フローチャートの作成
     root = flowchart.Root("Start")
 
     after_root_connector = flowchart.Connector("").add_from(root)
     input1 = flowchart.Input("User Input").add_next(after_root_connector)
 
-    action1 = BehaviorDetailAction("Action 1").add_from(after_root_connector)
-    action2 = BehaviorDetailAction("Action 2").add_from(action1)
-    action3 = BehaviorDetailAction("Action 3").add_from(action2)
-    action4 = BehaviorDetailAction("Action 4").add_from(action3)
+    action1 = BehaviorDetailAction("Action 1",
+                                   takt_time=1.0,
+                                   ).add_from(after_root_connector)
+    action2 = BehaviorDetailAction("Action 2",
+                                   takt_time=1.0,
+                                   ).add_from(action1)
 
-    action5 = BehaviorDetailAction("Action 5").add_from(action4)
-    action6 = BehaviorDetailAction("Action 6").add_from(action4)
+    # parallel
+    parallel1 = BehaviorParallel("Parallel 1").add_from(action2)
 
-    action7 = BehaviorDetailAction("Action 7").add_from(action4)
-    action8 = BehaviorDetailAction("Action 8").add_from(action7)
+    action3 = BehaviorDetailAction("Action 3",
+                                   takt_time=1.0,
+                                   )
+    parallel1.add_parallel_element(action3)
 
-    action9 = BehaviorDetailAction("Action 8")\
-        .add_from(action5)\
-        .add_from(action6)\
-        .add_from(action8)\
+    action4 = BehaviorDetailAction("Action 4",
+                                   takt_time=1.0,
+                                   )
+    parallel1.add_parallel_element(action4)
 
-    action10 = BehaviorDetailAction("Action 10").add_from(action9)
+    action5 = BehaviorDetailAction("Action 5",
+                                   takt_time=1.0,
+                                   )
+    action6 = BehaviorDetailAction("Action 6",
+                                   takt_time=1.0,
+                                   ).add_from(action5)
+    parallel1.add_parallel_element(action5)
+    # parallel end
 
-    loop_start = flowchart.LoopStart("Loop Start").add_from(action10)
+    action7 = BehaviorDetailAction("Action 7",
+                                   takt_time=1.0,
+                                   ).add_from(parallel1)
 
-    action11 = BehaviorDetailAction("Action 11").add_from(loop_start)
-    action12 = BehaviorDetailAction("Action 12").add_from(action11)
-    action13 = BehaviorDetailAction("Action 13").add_from(action12)
+    # loop
+    loop1 = BehaviorLoop("Loop 1",
+                         loop_count=3,
+                         ).add_from(action7)
 
-    loop_end = flowchart.LoopEnd("Loop End").add_from(action13)
+    action8 = BehaviorDetailAction("Action 8",
+                                   takt_time=1.0,
+                                   )
+    action9 = BehaviorDetailAction("Action 9",
+                                   takt_time=1.0,
+                                   ).add_from(action8)
 
-    action14 = BehaviorDetailAction("Action 14").add_from(loop_end)
+    loop1.set_loop_content(root_element=action8)
+    # loop end
 
-    decision = flowchart.Decision("Is Condition True?").add_from(action14)
-    action15 = BehaviorDetailAction("Action 15")
-    action16 = BehaviorDetailAction("Action 16")
+    action10 = BehaviorDetailAction("Action 10",
+                                    takt_time=1.0,
+                                    ).add_from(loop1)
 
-    decision.add_yes(action15)
-    decision.add_no(action16)
+    decision = BehaviorDecision("Is Condition True?").add_from(action10)
+    action11 = BehaviorDetailAction("Action 11",
+                                    takt_time=1.0,
+                                    )
+    action12 = BehaviorDetailAction("Action 12",
+                                    takt_time=1.0,
+                                    )
 
-    end = flowchart.Root("End").add_from(action15).add_from(action16)
-    
+    decision.add_yes(action11)
+    decision.add_no(action12)
+
+    end = flowchart.Root("End")
+
+    decision.add_next(end)
+
+    # BehaviorSummaryとしてまとめる
     behavior_start = flowchart.Root("Behavior Root")
     behavior_summary = BehaviorSummary(
         "Complex Example Summary",
@@ -108,5 +253,11 @@ if __name__ == "__main__":
 
     behavior_end = flowchart.Root("Behavior End").add_from(behavior_summary)
 
+    # フローチャート描画
     fc = flowchart.Flowchart(behavior_start)
-    fc.draw(filename="machine_behavior_example")
+    fc.draw(filename="machine_behavior_example",
+            # view=True,
+            view=False,
+            )
+
+    print(f"takt time: {behavior_summary.get_takt_time()}")
--- src/mechanical_design_lib/utils/flowchart.py
+++ src/mechanical_design_lib/utils/flowchart.py
@@ -36,9 +36,11 @@ class FlowchartElement:
 
     def get_nextlinks(self):
         return self._next_elements
+        # return copy.deepcopy(self._next_elements)
 
     def get_backlinks(self):
         return self._elements_backlinks
+        # return copy.deepcopy(self._elements_backlinks)
 
     def drop_nextlink(self, element):
         self._next_elements = [(next_element, label) for next_element,
@@ -80,11 +82,57 @@ class Action(FlowchartElement):
 
 
 class Decision(FlowchartElement):
+    def __init__(self, label):
+        super().__init__(label)
+
+        self._yes_root_element = None
+        self._no_root_element = None
+
+        self._yes_next_element = None
+        self._no_next_element = None
+
     def add_yes(self, element):
-        self.add_next(element, 'Yes')
+        self._yes_root_element = element
+        return self
 
     def add_no(self, element):
-        self.add_next(element, 'No')
+        self._no_root_element = element
+        return self
+
+    def add_yes_next(self, element):
+        self._yes_next_element = element
+        return self
+
+    def add_no_next(self, element):
+        self._no_next_element = element
+        return self
+
+    def post_process(self):
+        if self._yes_root_element is None \
+                or self._no_root_element is None:
+            raise ValueError('Decision element is not properly set.')
+
+        if self._yes_next_element is None or self._no_next_element is None:
+            next_links = self.get_nextlinks()
+            if len(next_links) > 1:
+                raise ValueError(
+                    'Decision element has multiple next elements.')
+
+            if self._yes_next_element is None:
+                self._yes_next_element = next_links[0][0]
+
+            if self._no_next_element is None:
+                self._no_next_element = next_links[0][0]
+
+        self._next_elements = []
+
+        self.add_next(self._yes_root_element, 'Yes')
+        get_last_element(self._yes_root_element).add_next(
+            self._yes_next_element)
+
+        self.add_next(self._no_root_element, 'No')
+        get_last_element(self._no_root_element).add_next(self._no_next_element)
+
 
     def add_to_graph(self, graph):
         graph.graph.node(self.id, self.label, shape='diamond')
@@ -111,12 +159,13 @@ class Subroutine(FlowchartElement):
         if self.is_parse_subroutine:
             first_element = self._subroutine_root_element
             new_first_element = self._subroutine_root_element.copy_to(
-                Connector(''))
+                Connector('Subroutine Start Connector'))
             for next_element, label in first_element.get_nextlinks():
                 next_element.replace_backlink(first_element, new_first_element)
 
             last_element = get_last_element(new_first_element)
-            new_last_element = last_element.copy_to(Connector(''))
+            new_last_element = last_element.copy_to(
+                Connector('Subroutine End Connector'))
             for backlink in last_element.get_backlinks():
                 backlink.replace_nextlink(last_element, new_last_element)
 
@@ -129,6 +178,67 @@ class Subroutine(FlowchartElement):
                 backlink.drop_nextlink(self)
 
             self._subroutine_root_element = new_first_element
+
+
+class Loop(FlowchartElement):
+    def __init__(self, label: str, loop_count: int):
+        super().__init__(label)
+        self._loop_start = LoopStart(f'{label} Start\n (n= {loop_count})')
+        self._loop_end = LoopEnd(f'{label} End')
+
+        self._loop_content = None
+        self._loop_count = loop_count
+
+    def set_loop_content(self, root_element):
+        self._loop_content = root_element
+
+    @property
+    def label(self):
+        return f"{self._label}\n (n= {self._loop_count})"
+
+    def post_process(self):
+        self._loop_start.add_next(self._loop_content)
+        last_elem = get_last_element(self._loop_content)
+        last_elem.add_next(self._loop_end)
+
+        tmp_backlinks = self.get_backlinks()
+        for backlink in self.get_backlinks():
+            backlink.replace_nextlink(self, self._loop_start)
+        self._elements_backlinks = tmp_backlinks
+
+        tmp_nextlinks = self.get_nextlinks()
+        for next_element, label in self.get_nextlinks():
+            next_element.replace_backlink(self, self._loop_end)
+        self._next_elements = tmp_nextlinks
+
+
+class Parallel(FlowchartElement):
+    def __init__(self, label):
+        super().__init__(label)
+        self._parallel_elements = []
+
+        self._parallel_start_connector = Connector(f'{label} Start Connector')
+        self._parallel_end_connector = Connector(f'{label} End Connector')
+
+    def add_parallel_element(self, root_element):
+        self._parallel_elements.append(root_element)
+        return self
+
+    def post_process(self):
+        for element in self._parallel_elements:
+            self._parallel_start_connector.add_next(element)
+            last_elem = get_last_element(element)
+            last_elem.add_next(self._parallel_end_connector)
+
+        tmp_backlinks = self.get_backlinks()
+        for backlink in self.get_backlinks():
+            backlink.replace_nextlink(self, self._parallel_start_connector)
+        self._elements_backlinks = tmp_backlinks
+
+        tmp_nextlinks = self.get_nextlinks()
+        for next_element, label in self.get_nextlinks():
+            next_element.replace_backlink(self, self._parallel_end_connector)
+        self._next_elements = tmp_nextlinks
 
 
 class Input(FlowchartElement):
@@ -163,6 +273,8 @@ def get_last_element(element):
 
     while queue:
         current_element = queue.pop(0)
+        if current_element in visited:
+            continue
         visited.add(current_element)
 
         next_elements = current_element.get_nextlinks()
@@ -188,8 +300,11 @@ class ElementIterator:
 
         while queue:
             current_element = queue.pop(0)
+            if current_element in visited:
+                continue
             visited.add(current_element)
 
+            print(f"Current element: {current_element.label}")
             yield current_element
 
             next_elements = current_element.get_nextlinks()
@@ -203,6 +318,17 @@ class ElementIterator:
                     queue.append(backlink)
 
 
+class ElementsCompiler:
+    @staticmethod
+    def compile(root_element) -> FlowchartElement:
+        # root_element から走査して、新しい FlowchartElement を作成する
+        new_root_element = copy.deepcopy(root_element)
+        for element in ElementIterator(new_root_element):
+            element.post_process()
+
+        return new_root_element
+
+
 class Flowchart:
     def __init__(self, root_element):
         self.root_element = root_element
@@ -213,21 +339,27 @@ class Flowchart:
 
     def _post_process(self):
         # for element in self._get_elements():
-        for element in ElementIterator(self.root_element):
-            element.post_process()
+        print("Post processing")
+        # for element in ElementIterator(self.root_element):
+        #     element.post_process()
+        self.root_element = ElementsCompiler.compile(self.root_element)
+        print("Post processing done")
 
-    def draw(self, filename='flowchart'):
+    def draw(self, filename='flowchart', view=True):
         if not self.root_element:
             raise ValueError('Root element is not set.')
         self._post_process()
 
         self._add_elements_to_graph(self.root_element)
-        self.graph.render(filename, view=True)
+        self.graph.render(filename,
+                          view=view,
+                          )
 
     def _add_elements_to_graph(self, element):
         if element.id in self.visited_nodes:
             return
 
+        print(f"Adding element: {element.label}")
         element.add_to_graph(self)
         self.visited_nodes.add(element.id)
 
@@ -267,20 +399,19 @@ if __name__ == '__main__':
     decision = Decision('Is Condition True?').add_from(action1)
 
     action2 = Action('Action 2')
-    action3 = Action('Action 3')
-    decision.add_yes(action2)
-    decision.add_no(action3)
-
     subroutine = Subroutine('Subroutine', sub_root, is_parse_subroutine=True,
                             ).add_from(action2)
 
+    decision.add_yes(action2)
+
+    action3 = Action('Action 3')
     loop_start = LoopStart('Loop Start').add_from(action3)
     action4 = Action('Action 4').add_from(loop_start)
     loop_end = LoopEnd('Loop End').add_from(action4)
 
-    end = Root('End')
-    end.add_from(subroutine)
-    end.add_from(loop_end)
+    decision.add_no(action3)
+
+    end = Root('End').add_from(decision)
 
     # フローチャート描画
     flowchart = Flowchart(root)
